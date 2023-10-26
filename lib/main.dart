6a9f51bdfb38c25d88aa75c70ddd341f5db9c66d
mathimatech <matimatech@proton.me>
2023-10-26 17:40:00 +0700

    Add utils

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+import './screens/products_overview_screen.dart';
+import './screens/product_detail_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'MyShop',
+      theme: ThemeData(
+        primarySwatch: Colors.indigo,
+        hintColor: Colors.amber,
+        fontFamily: 'Lato',
+      ),
+      home: ProductsOverviewScreen(),
+      routes: {
+        ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
+      },
+    );
+  }
+}
